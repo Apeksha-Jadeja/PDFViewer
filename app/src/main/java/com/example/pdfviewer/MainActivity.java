dd076034dd0df16303332d07379d660ab35af146
Apeksha Jadeja <81768737+Apeksha-Jadeja@users.noreply.github.com>
2021-07-25 14:53:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pdfviewer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pdfviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pdfView=findViewById(R.id.pdfView);
+        pdfView.fromAsset("resume.pdf").load();
+
+    }
+}
